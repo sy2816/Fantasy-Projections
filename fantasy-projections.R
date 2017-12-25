bbf7d8f8d2f5609eb576135abfb2886c05952b84
sy2816 <syork2816@gmail.com>
2017-12-24 22:43:12 -0500

    finalizing some code.

--- fantasy-projections.R
+++ fantasy-projections.R
@@ -1,7 +1,9 @@
 library(rvest)
 library(dplyr)
+library(ggplot2)
+library(highcharter)
 
-week <- c(1:11)
+week <- c(1:16)
 index <- c(0, 40, 60, 80, 120, 140, 160, 200, 220, 240, 260)
 
 cat <- 0 #Signifies QBs
@@ -47,8 +49,7 @@ colnames(projqb) <- c("player", "opp", "status", "catchAttempt", "passYards", "p
                       ,"rushTD", "rec", "recYards", "recTD", "projPoints", "week")
 
 # Actual scores
-
-weekReal <- c(1:11)
+weekReal <- c(1:16)
 cat <- 0
 indexReal <- c(0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600)
 
@@ -100,7 +101,16 @@ for(i in c(1:length(players))){
   dataReal <- filter(realqb, player == players[i])
   dataReal <- dataReal[,c(1,17,18)]
   
+  #Eliminate invalid characters from column
+  invalidChars <- grep("--", dataReal$realPoints)
+  if(length(invalidChars > 0)){
+    dataReal <- dataReal[-invalidChars, ]
+  }
+  
   joinedData <- inner_join(dataProj, dataReal, by = "week")
+
+  # Also eliminate games where there was 0 proj and 0 real points, since it implies they player never actually played.
+  joinedData <- subset(joinedData, joinedData$projPoints != "0" & joinedData$realPoints != "0")
   
   joinedData$projPoints <- as.numeric(joinedData$projPoints)
   joinedData$realPoints <- as.numeric(joinedData$realPoints)
@@ -114,3 +124,30 @@ for(i in c(1:length(players))){
   summaryData <- rbind(summaryData, bindDat)
 }
 summaryData$meanDiff <- round(summaryData$meanDiff, digits = 1)
+summaryData <- na.omit(summaryData)
+
+
+ggplot()+geom_point(data = summaryData, aes(x = player, y = medianDiff))
+
+# some cleaning of the play data names before I graph.
+summaryData$player <- as.character(summaryData$player)
+for(i in c(1:length(summaryData$player))){
+  comma <- gregexpr(pattern = ",", summaryData$player[i])[[1]][1]
+  summaryData$player[i] <- substr(x = summaryData$player[i], start = 1, stop = (comma-1))
+  summaryData$player[i] <- gsub(pattern = "*", replacement = "", x = summaryData$player[i], fixed = TRUE)
+  
+}
+
+hchart(object = summaryData, "scatter", hcaes(x = player, y = medianDiff))%>%
+  hc_plotOptions(scatter = list(
+    dataLabels = list(enabled = TRUE,
+                      format = "{key}"
+  ))) %>%
+  hc_yAxis(title = list(text = "Median Difference")) %>%
+  hc_title(text = "QB median difference between projected points and actual points") %>%
+  hc_xAxis(visible = FALSE) %>%
+  hc_tooltip(pointFormat = '<span style="color:{point.color}">\u25CF</span> <b>{point.name}</b> <br>
+             Median Difference: <b>{point.y}</b><br/>',
+             headerFormat = '<span style="font-size: 10px"></span>') %>%
+  hc_chart(zoomType = "xy")
+
